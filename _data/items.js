80829e6406c6c3c1680a44a6e1af6e430f890948
Jonas Luebbers <jonaskluebbers@gmail.com>
2021-04-14 22:46:43 -0400

    Add videos

---
+++ _data/items.js
@@ -0,0 +1,124 @@
+module.exports = [
+  {
+    src: "/assets/images/butterfly-final.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/c2c-hero-animation-final.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/frameio-after-effects-hero-animation.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/frameio-slider.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/squarespace.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/thumbnail.png",
+  },
+  {
+    src: "/assets/images/sample.png",
+  },
+  {
+    src: "/assets/images/slideshow.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/carmine-demo.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/suhama-demo.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/classon-demo.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/organic-grid.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/scroll-sections.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/index-00.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/index-01.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/index-02.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/index-03.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/comp-shop.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/cows.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/flare.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/interlace.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/distortion.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/chromatic.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/halftone.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/slice.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/water.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/labspace.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/map-blog-2.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/music-blog.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/cross-hatching.mp4",
+    isVideo: true,
+  },
+  {
+    src: "/assets/images/planets.mp4",
+    isVideo: true,
+  },
+];
